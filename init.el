6a917a7a40c6142d25fe2f5e25595aeac74a3aa3
Rickard Claesson A <rickard.a.claesson@ericsson.com>
2019-02-26 11:07:25 +0100

    add toml and dockerfile mode

--- init.el
+++ init.el
@@ -187,6 +187,13 @@
   (add-hook 'prog-mode-hook #'smartparens-mode)
   )
 
+(use-package toml-mode
+  :ensure t
+  )
+
+(use-package dockerfile-mode
+  :ensure t)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
